26438a513069375283e1057ded7121517924af52
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-07-07 14:27:52 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ module.exports={
     ConfirmationIntend(event,context){
         console.log('triggering ConfirmationIntend');
         console.log("facebook Result" + event.result.contexts);
+        var phoneNumber=event.result.contexts[0].phone-number;
+        var Name=event.result.contexts[0].Firstname+""+event.result.contexts[0].lastName;
+        Console.log(JSON.stringify(phoneNumber));
+        Console.log(JSON.stringify(Name));
+        
     }
 }
